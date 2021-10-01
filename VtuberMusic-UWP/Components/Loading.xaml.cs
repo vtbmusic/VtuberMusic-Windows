8248cff77926c29579a3431877dd8b4be89a7ddc
Misaka_L <misaka-l@vtbmusic.icu>
2021-10-01 23:30:09 +0800

    add: 新的加载动画 change: 修改播放历史页 Margin

---
+++ VtuberMusic-UWP/Components/Loading.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//https://go.microsoft.com/fwlink/?LinkId=234236 上介绍了“用户控件”项模板
+
+namespace VtuberMusic_UWP.Components {
+    public sealed partial class Loading : UserControl {
+        public Loading() {
+            this.InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e) {
+            this.LoadingStoryboard.Begin();
+        }
+    }
+}
