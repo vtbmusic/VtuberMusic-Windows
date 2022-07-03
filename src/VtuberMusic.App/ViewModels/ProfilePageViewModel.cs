b159167ba8068e4edae1d0fce4c7848bba738d56
Misaka-L <misaka-l@vtbmusic>
2022-07-03 19:35:39 +0800

    Profile 页完工

--- src/VtuberMusic.App/ViewModels/ProfilePageViewModel.cs
+++ src/VtuberMusic.App/ViewModels/ProfilePageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Toolkit.Mvvm.DependencyInjection;
 using Microsoft.Toolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using VtuberMusic.Core.Models;
 using VtuberMusic.Core.Services;
@@ -10,8 +11,12 @@ namespace VtuberMusic.App.ViewModels {
 
         public IAsyncRelayCommand LoadCommand { get; }
 
+        public ObservableCollection<Playlist> CreatePlaylist = new ObservableCollection<Playlist>();
+        public ObservableCollection<Playlist> SubPlaylist = new ObservableCollection<Playlist>();
         public Profile Profile { get => profile; set => SetProperty(ref profile, value); }
         private Profile profile;
+        public Playlist FavouritePlaylist { get => favouritePlaylist; set => SetProperty(ref favouritePlaylist, value); }
+        private Playlist favouritePlaylist;
 
         public ProfilePageViewModel() {
             LoadCommand = new AsyncRelayCommand(LoadDataAsync);
@@ -19,6 +24,18 @@ namespace VtuberMusic.App.ViewModels {
 
         private async Task LoadDataAsync() {
             Profile = (await _vtuberMusicService.GetProfile(Profile.userId)).Data.profile;
+
+            var favouritePlaylistResponse = await _vtuberMusicService.GetFavouriteMusicsPlaylist("song", Profile.userId);
+            var subPlaylistResponse = await _vtuberMusicService.GetSubPlaylist(Profile.userId);
+            var createPlaylistResponse = await _vtuberMusicService.GetCreatePlaylist(Profile.userId);
+
+            SubPlaylist.Clear();
+            foreach (var item in subPlaylistResponse.Data) SubPlaylist.Add(item);
+
+            CreatePlaylist.Clear();
+            foreach (var item in createPlaylistResponse.Data) CreatePlaylist.Add(item);
+
+            FavouritePlaylist = favouritePlaylistResponse.Data.playlist;
         }
     }
 }
