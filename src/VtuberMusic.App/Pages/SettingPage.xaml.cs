1a3dc9d26041ed94470ef01116241906e7aea2af
Misaka-L <misaka-l@vtbmusic.icu>
2022-09-04 03:35:31 +0800

    添加: 设置页显示应用程序包版本号

--- src/VtuberMusic.App/Pages/SettingPage.xaml.cs
+++ src/VtuberMusic.App/Pages/SettingPage.xaml.cs
@@ -3,6 +3,7 @@ using Microsoft.UI.Xaml.Controls;
 using System;
 using VtuberMusic.App.Dialogs;
 using VtuberMusic.App.ViewModels.Pages;
+using Windows.ApplicationModel;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
@@ -15,6 +16,9 @@ public sealed partial class SettingPage : Page {
 
     public SettingPage() {
         InitializeComponent();
+
+        var version = Package.Current.Id.Version;
+        VersionText.Text = $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision} © VtuberMusic {DateTimeOffset.Now.Year}";
     }
 
     private async void PrivacyDialog_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e) {
