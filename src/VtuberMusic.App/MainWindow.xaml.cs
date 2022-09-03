7d3436a96c0df316663e75323af70c48c7e5003f
Misaka-L <misaka-l@vtbmusic.icu>
2022-09-04 03:00:48 +0800

    修复: 修复了登录后也无法进入主页的问题

--- src/VtuberMusic.App/MainWindow.xaml.cs
+++ src/VtuberMusic.App/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.UI.Xaml;
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.UI.Xaml;
 using VtuberMusic.App.Helper;
 using VtuberMusic.App.Pages;
+using VtuberMusic.Core.Services;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -11,18 +13,24 @@ namespace VtuberMusic.App;
 /// </summary>
 public sealed partial class MainWindow : Window {
     private readonly WindowBackdropHelper _backdropHelper;
+    private readonly IAuthorizationService _authorizationService = Ioc.Default.GetRequiredService<IAuthorizationService>();
 
     public MainWindow() {
         InitializeComponent();
         _backdropHelper = new WindowBackdropHelper(this);
         _backdropHelper.TrySetMicaBackdrop();
 
-        Title = "VtuberMusic";
-        ExtendsContentIntoTitleBar = true;
+        this.Title = "VtuberMusic";
+        this.ExtendsContentIntoTitleBar = true;
 
         App.MainWindow = this;
         App.RootFrame = RootFrame;
-        DispatcherHelper.Init(DispatcherQueue);
-        RootFrame.Navigate(typeof(LoginPage));
+        DispatcherHelper.Init(this.DispatcherQueue);
+
+        if (_authorizationService.IsLogin) {
+            RootFrame.Navigate(typeof(LoginPage));
+        } else {
+            RootFrame.Navigate(typeof(MainPage));
+        }
     }
 }
