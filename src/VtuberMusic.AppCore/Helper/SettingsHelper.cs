0b4b04a1311e2f33f73d7be88899d50b709789d9
Misaka-L <misaka-l@vtbmusic.icu>
2022-09-05 21:25:15 +0800

    修复: 修复 SettingsHelper 无法保存 Enum 的问题

--- src/VtuberMusic.AppCore/Helper/SettingsHelper.cs
+++ src/VtuberMusic.AppCore/Helper/SettingsHelper.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.CompilerServices;
+﻿using Newtonsoft.Json;
+using System;
+using System.Runtime.CompilerServices;
 using VtuberMusic.AppCore.Enums;
 using Windows.Storage;
 
@@ -14,14 +16,27 @@ public class SettingsHelper {
         set => SetSetting(value);
     }
 
-    public static void SetSetting(object value , [CallerMemberName] string key = "") => ApplicationData.Current.LocalSettings.Values[key] = value;
+    public static void SetSetting(object value , [CallerMemberName] string key = "") {
+        if (value is Enum) {
+            ApplicationData.Current.LocalSettings.Values[key] = (int)value;
+            return;
+        }
+
+        ApplicationData.Current.LocalSettings.Values[key] = value;
+    }
 
     public static T GetSetting<T>(T defaultValue , [CallerMemberName] string key = "") {
-        if (ApplicationData.Current.LocalSettings.Values[key] is T data) {
-            return data;
-        } else {
-            return defaultValue;
+        var raw = ApplicationData.Current.LocalSettings.Values[key];
+        switch (raw) {
+            case T:
+                return (T)raw;
+            case int:
+                if (typeof(T).BaseType == typeof(Enum))
+                    return (T)raw;
+                break;
         }
+
+        return default;
     }
 
     public static T GetSetting<T>([CallerMemberName] string key = "") => (T)ApplicationData.Current.LocalSettings.Values[key];
