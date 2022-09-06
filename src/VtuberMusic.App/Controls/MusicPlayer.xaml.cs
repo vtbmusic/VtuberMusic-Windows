8f9d5faac0cbf7cf8f0af59ff73e1faa7b04608b
Misaka-L <misaka-l@vtbmusic.icu>
2022-09-06 14:29:03 +0800

    添加: 音量调节 #47 维护: 将 Playing 和 MainPlayer 控件的事件注册改为注册消息，并删除对 Service 的引用

--- src/VtuberMusic.App/Controls/MusicPlayer.xaml.cs
+++ src/VtuberMusic.App/Controls/MusicPlayer.xaml.cs
@@ -1,54 +1,91 @@
 ﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using CommunityToolkit.Mvvm.Messaging;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 using System;
+using VtuberMusic.App.Helper;
 using VtuberMusic.App.ViewModels;
+using VtuberMusic.AppCore.Messages;
 using VtuberMusic.AppCore.Services;
 
 //https://go.microsoft.com/fwlink/?LinkId=234236 上介绍了“用户控件”项模板
 
 namespace VtuberMusic.App.Controls;
 public sealed partial class MusicPlayer : UserControl {
-    private bool isMove = false;
+    private bool isPositionMove = false;
+    private bool isVolumeMove = false;
 
     public event EventHandler RequsetShowPlaying;
 
     private readonly MusicPlayerViewModel ViewModel = Ioc.Default.GetRequiredService<MusicPlayerViewModel>();
-    private readonly IMediaPlayBackService _mediaPlayBackService = Ioc.Default.GetRequiredService<IMediaPlayBackService>();
 
     public MusicPlayer() {
         InitializeComponent();
 
-        PositionSlider.AddHandler(PointerPressedEvent, new PointerEventHandler(Slider_PointerPressed), true);
-        PositionSlider.AddHandler(PointerReleasedEvent, new PointerEventHandler(Slider_PointerReleased), true);
+        PositionSlider.AddHandler(PointerPressedEvent, new PointerEventHandler(PositionSlider_PointerPressed), true);
+        PositionSlider.AddHandler(PointerReleasedEvent, new PointerEventHandler(PositionSlider_PointerReleased), true);
+
+        VolumeSlider.AddHandler(PointerPressedEvent, new PointerEventHandler(VolumeSlider_PointerPressed), true);
+        VolumeSlider.AddHandler(PointerReleasedEvent, new PointerEventHandler(VolumeSlider_PointerReleased), true);
+
+        isPositionMove = true;
+        isVolumeMove = true;
+
+        VolumeSlider.Value = ViewModel.Volume;
+        PositionSlider.Value = ViewModel.PlayerPosition.TotalSeconds;
+        PositionSlider.Maximum = ViewModel.PlayerDuration.TotalSeconds;
+
+        isPositionMove = false;
+        isVolumeMove = false;
     }
 
-    private void MediaPlaybackService_PositionChanged(object sender, TimeSpan e) => this.DispatcherQueue.TryEnqueue(delegate {
-        try {
-            if (PositionSlider != null & !isPositionMove) {
-            PositionSlider.Value = e.TotalSeconds;
-        }
-        } catch { }
-    });
+    #region 处理歌曲进度条事件
+    private void PositionSlider_PointerPressed(object sender, PointerRoutedEventArgs e) => isPositionMove = true;
 
-    private void Slider_PointerPressed(object sender, PointerRoutedEventArgs e) => isMove = true;
-
-    private void Slider_PointerReleased(object sender, PointerRoutedEventArgs e) {
-        if (isMove) {
-            _mediaPlayBackService.Position = TimeSpan.FromSeconds(PositionSlider.Value);
+    private void PositionSlider_PointerReleased(object sender, PointerRoutedEventArgs e) {
+        if (isPositionMove) {
+            ViewModel.PlayerPosition = TimeSpan.FromSeconds(PositionSlider.Value);
         }
 
-        isMove = false;
+        isPositionMove = false;
     }
+    #endregion
+
+    #region 音量条事件
+    private void VolumeSlider_PointerReleased(object sender, PointerRoutedEventArgs e) => isVolumeMove = false;
+    private void VolumeSlider_PointerPressed(object sender, PointerRoutedEventArgs e) => isVolumeMove = true;
+
+    private void VolumeSlider_ValueChanged(object sender, Microsoft.UI.Xaml.Controls.Primitives.RangeBaseValueChangedEventArgs e) {
+        if (isVolumeMove)
+            ViewModel.Volume = e.NewValue;
+    }
+    #endregion
 
     private void ShowPlaying_Click(object sender, RoutedEventArgs e) => RequsetShowPlaying?.Invoke(this, null);
 
     private void UserControl_Loaded(object sender, RoutedEventArgs e) {
-        _mediaPlayBackService.PositionChanged += MediaPlaybackService_PositionChanged;
+        ViewModel.IsActive = true;
+
+        WeakReferenceMessenger.Default.Register(this, delegate (object sender, PlaybackVolumeChangedMessage message) {
+            DispatcherHelper.TryRun(delegate {
+                if (!isVolumeMove) {
+                    VolumeSlider.Value = message.Value;
+                }
+            });
+        });
+
+        WeakReferenceMessenger.Default.Register(this, delegate (object sender, PlaybackPositionChangedMessage message) {
+            DispatcherHelper.TryRun(delegate {
+                if (!isPositionMove) {
+                    PositionSlider.Value = message.Value.Position.TotalSeconds;
+                    PositionSlider.Maximum = message.Value.Duration.TotalSeconds;
+                }
+            });
+        });
     }
 
     private void UserControl_Unloaded(object sender, RoutedEventArgs e) {
-        _mediaPlayBackService.PositionChanged -= MediaPlaybackService_PositionChanged;
+        WeakReferenceMessenger.Default.UnregisterAll(this);
     }
 }
---
+++ src/VtuberMusic.App/Converters/DoubleToPercentageConverter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VtuberMusic.App.Converters;
+public class DoubleToPercentageConverter : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, string language) {
+        if (value is double doubleValue)
+            return doubleValue.ToString("P0");
+
+        return DependencyProperty.UnsetValue;
+    }
+    public object ConvertBack(object value, Type targetType, object parameter, string language) {
+        return DependencyProperty.UnsetValue;
+    }
+}
