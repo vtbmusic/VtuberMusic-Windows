9cb9e411bf9d826cf5806b159eef3a3811e2cb92
Misaka-L <misaka-l@vtbmusic.icu>
2022-09-06 15:25:49 +0800

    优化: 优化了当初始页为 "我喜欢的音乐" 时进入初始页的延迟

--- src/VtuberMusic.App/ViewModels/Pages/MainPageViewModel.cs
+++ src/VtuberMusic.App/ViewModels/Pages/MainPageViewModel.cs
@@ -63,6 +63,9 @@ public partial class MainPageViewModel : ObservableObject {
         this.NavigationItems.Insert(2, createNavgationItem(typeof(PlaylistPage), "我喜欢的音乐", new FontIcon { FontFamily = new FontFamily("Segoe Fluent Icons"), Glyph = "\uE006" },
             new PlaylistPageArg { Playlist = likeMusicsPlaylist.Data.playlist, PlaylistType = PlaylistType.LikeMusics }));
 
+        if (SettingsHelper.DefaultNavigationPage == DefaultNavigationPage.LikeMusic)
+            NavigationHelper.Navigate<PlaylistPage>(new PlaylistPageArg { Playlist = likeMusicsPlaylist.Data.playlist, PlaylistType = PlaylistType.LikeMusics });
+
         this.NavigationItems.Add(new NavigationViewItemHeader { Content = "我创建的歌单" });
         var createPlaylists = (await _vtuberMusicService.GetCreatePlaylist()).Data;
         foreach (var item in createPlaylists) {
@@ -74,9 +77,6 @@ public partial class MainPageViewModel : ObservableObject {
         foreach (var item in subPlaylists) {
             this.NavigationItems.Add(createNavgationItem(typeof(PlaylistPage), item.name, new SymbolIcon(Symbol.MusicInfo), new PlaylistPageArg { Playlist = item }));
         }
-
-        if (SettingsHelper.DefaultNavigationPage == DefaultNavigationPage.LikeMusic)
-            NavigationHelper.Navigate<PlaylistPage>(new PlaylistPageArg { Playlist = likeMusicsPlaylist.Data.playlist, PlaylistType = PlaylistType.LikeMusics });
     }
 
     private static NavigationViewItem createNavgationItem(Type type, string title, IconElement icon, object args = null) => new() { Icon = icon, Content = title, Tag = new NavigationTag { Type = type, Args = args } };
